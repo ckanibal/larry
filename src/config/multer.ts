ec99c81040f79f1321b19b357bffe41bc1e69076
Kanibal <frustrum@pictor.uberspace.de>
2018-03-28 20:02:53 +0200

    Limit upload size.

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -5,5 +5,9 @@ import * as multer from "multer";
 /**
  * multer file upload configuration
  */
-const upload = multer({ dest: "./tmp/", });
+
+// 100MB default max filesize
+const MAX_FILESIZE = parseInt(process.env.MAX_SIZE) || 100 * 1024 * 1024;
+
+const upload = multer({dest: "./tmp/", limits: {fileSize: MAX_FILESIZE}});
 export = upload;
