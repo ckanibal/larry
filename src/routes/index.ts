49ac5e211a44a16bc538bee954e7ff7539fd105f
Kanibal <frustrum@pictor.uberspace.de>
2018-05-25 20:00:33 +0200

    Remove BaseController.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,12 +5,10 @@ import { UploadController } from "./uploads/UploadController";
 import { CommentController } from "./comments/CommentController";
 import { AuthController } from "./authentication/AuthController";
 import { MediaController } from "./media/MediaController";
-import { BaseController } from "./BaseController";
 import auth = require("../config/auth");
 
 const router = express.Router();
 // router.use("/api", require("./api"));
-router.use("/", new BaseController().router);
 router.use("/auth", new AuthController().router);
 router.use("/media", new MediaController().router);
 router.use("/uploads", new UploadController().router);
